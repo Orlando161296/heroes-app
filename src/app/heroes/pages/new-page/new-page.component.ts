82211e8d20ac892702f1f498151277abc9604c29
Orlando Rojas <josuerojas161296@gmail.com>
2023-10-01 18:09:35 -0400

    implementación de un CRUD visto en la sección 15 del curso de Angular.

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,16 +1,124 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { Hero, Publisher } from './../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { filter, switchMap, tap } from 'rxjs';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit{
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', {nonNullable: true}),
+    publisher: new FormControl<Publisher>( Publisher.DCComics ),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
 
   public publishers = [
-    { id: 'DC Comics', desc: 'DC - Comics'},
-    { id: 'Marvel Comics', desc: 'Marvel - Comics'},
-  ]
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' },
+  ];
+
+  constructor(private heroesService: HeroesService,
+    private ActivatedRoute: ActivatedRoute,
+    private router: Router,
+    private snackbar: MatSnackBar,
+    private dialog: MatDialog,
+    ){}
+
+
+  get currentHero(): Hero{
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+  ngOnInit(): void {
+
+    if( !this.router.url.includes('edit')) return;
+
+    this.ActivatedRoute.params
+    .pipe(
+      switchMap(({ id }) => this.heroesService.getHeroById( id )),
+    ).subscribe( hero => {
+      if(!hero ) return this.router.navigateByUrl('/');
+
+      this.heroForm.reset( hero );
+      return;
+    })
+
+
+  }
+
+  onSubmit():void{
+    if(this.heroForm.invalid) return;
+
+    if( this.currentHero.id ){
+      this.heroesService.updateHero( this.currentHero )
+      .subscribe( hero => {
+        this.showSnabkbar(`${ hero.superhero} updated!`)
+      });
+
+      return;
+    }
+
+    this.heroesService.addHero( this.currentHero)
+    .subscribe(hero =>{
+      this.router.navigate(['/heroes/edit', hero.id]);
+      this.showSnabkbar(`${ hero.superhero} updated!`);
+    })
+
+  }
+
+
+  onDeleteHero(){
+    if( !this.currentHero.id) throw Error('Hero id is required')
+
+    const dialogRef = this.dialog.open( ConfirmDialogComponent, {
+      data: this.heroForm.value
+    });
+
+    dialogRef.afterClosed()
+    .pipe(
+      filter( (result: boolean) =>  result),
+      switchMap( () => this.heroesService.deleteHeroById( this.currentHero.id ) ),
+      filter( (wasDelete: boolean) => wasDelete),
+    )
+    .subscribe(result => {
+      console.log({ result });
+
+      this.router.navigate(['/heroes'])
+    })
+
+
+    // dialogRef.afterClosed().subscribe(result => {
+
+    //   if( !result ) return;
+
+    //   console.log('Delete')
+
+    //   this.heroesService.deleteHeroById( this.currentHero.id )
+    //   .subscribe( wasDelete => {
+    //     if( wasDelete )
+    //     this.router.navigate(['/heroes'])
+    //   })
+    // });
+  }
+
+
+  showSnabkbar(message: string):void{
+    this.snackbar.open( message, 'done',{ duration: 2500})
+  }
+
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs'
+import { Observable, catchError, map, of } from 'rxjs'
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments';
 
@@ -26,6 +26,42 @@ export class HeroesService {
 
   getSuggestions( query: string ):Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${ query }&_limit=6`)
+  }
+
+  /**
+   * enpoint put
+   * Método para agregar un nuevo héroe al backend.
+   * @param hero Recibe un Héroe como parametro.
+   * @returns retorna un observable con un nuevo héroe.
+   */
+  addHero( hero: Hero ): Observable<Hero>{
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero)
+  }
+
+  /**
+   * endpoint patch
+   * Método para modificar alguna propiedad del héroe.
+   * @param hero Recibe un héroe con sus propiedades.
+   * @returns retorna un héroe con la nueva propiedad asignada.
+   */
+  updateHero( hero: Hero ): Observable<Hero>{
+    if(!hero.id ) throw Error('Hero id is required');
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${ hero.id}`, hero)
+  }
+
+
+  /**
+   * endpoint delete
+   *Método para eliminar un héroe.
+   * @param id recibe un Id de un héroe
+   * @returns retorna verdadero si es un 200 y no cae en error y false si tiene algun error.
+   */
+  deleteHeroById( id: string ): Observable<boolean>{
+    return this.http.delete(`${this.baseUrl}/heroes/${ id }`)
+    .pipe(
+      map( resp => true ),
+      catchError( err => of(false)),
+    );
   }
 
 }
