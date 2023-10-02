db6aeebd861bf7b2d0fdbef3d2bc80c0a49437f1
Orlando Rojas <josuerojas161296@gmail.com>
2023-10-02 10:55:09 -0400

    Sección 16 implementación de protección de Rutas, Guards CanMatchFn, CanActivedFn nueva versión A16.

---
+++ src/app/auth/guards/public.guard.ts
@@ -0,0 +1,42 @@
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  CanMatchFn,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+} from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { Observable, map, tap } from 'rxjs';
+import { inject } from '@angular/core';
+
+const checkAuthStatus = (): boolean | Observable<boolean> => {
+  //se inyectan el AuthService y el Router
+  const authService: AuthService = inject(AuthService);
+  const router: Router = inject(Router);
+
+  return authService.checkAuthentication().pipe(
+    tap((isAuthenticated) => {
+      if (isAuthenticated) {
+        router.navigate(['/']);
+      }
+    }),
+    map( isAuthenticated => !isAuthenticated )
+  );
+};
+//TODO: REVISAR LA DOCUMENTACION DE ANGULAR CON CANACTIVATEFN Y CANMATCHGUARD PARA UNA MEJOR COMPRENSION DE LA SECCIÓN
+//No hay necesidad de crear una clase, simplemente definiendo una función flecha y exportándola podemos utilizar sus funcionalidades de guard en el app-routing
+export const canActivateGuardPublic: CanActivateFn = ( //Hay que tener en cuenta el tipado CanActiveFn
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  return checkAuthStatus();
+};
+
+export const canMatchGuardPublic: CanMatchFn = ( //Tipado CanMatchFN
+  route: Route,
+  segments: UrlSegment[]
+) => {
+  return checkAuthStatus();
+};
